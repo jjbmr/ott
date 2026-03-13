e21a3dc870a36aa065fffd07d80ae8f720303b9a
JBMR SPORTS <jbmrsports@JBMRs-Mac-mini.local>
2026-03-13 14:25:11 +0530

    Support process.env.PORT for deployment

--- server.js
+++ server.js
@@ -255,7 +255,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
